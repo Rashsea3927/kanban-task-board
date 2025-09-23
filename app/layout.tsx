9bb4a353564766140758e75002166bb9f9a4316f
Rashsea <kaji.dev3927@gmail.com>
2025-09-23 21:39:00 +0900

    refactor(layout): enforce single quotes instead of double quotes and change order of imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import '@/styles/globals.css';
+import type { Metadata } from 'next';
 import { Plus_Jakarta_Sans } from 'next/font/google';
 
 const plusJakartaSans = Plus_Jakarta_Sans({
@@ -18,6 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <html lang='en'>
       <body className={`${plusJakartaSans.className} antialiased`}>{children}</body>
     </html>
   );

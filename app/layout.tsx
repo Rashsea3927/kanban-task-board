eb0cc214dd1d32969627272f2a5cf777673a879c
Rashsea <kaji.dev3927@gmail.com>
2025-09-23 21:26:41 +0900

    style(layout): apply new font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Plus_Jakarta_Sans } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const plusJakartaSans = Plus_Jakarta_Sans({
+  variable: '--font-plus-jakarta-sans',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
@@ -23,12 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${plusJakartaSans.className} antialiased`}>{children}</body>
     </html>
   );
 }
